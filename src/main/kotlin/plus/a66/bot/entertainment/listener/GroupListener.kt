02887096b8a5ec369e59b28a73f27a5852e4e8af
VarleyT <1963460510@qq.com>
2023-01-21 19:30:01 +0800

    新增 抖音智能解析功能 修复 抖音解析资源过多造成无法发送的问题

--- src/main/kotlin/plus/a66/bot/entertainment/listener/GroupListener.kt
+++ src/main/kotlin/plus/a66/bot/entertainment/listener/GroupListener.kt
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ReUtil
 import cn.hutool.http.HttpStatus
 import cn.hutool.http.HttpUtil
 import com.alibaba.fastjson2.JSON
+import kotlinx.coroutines.launch
 import love.forte.simboot.annotation.Filter
 import love.forte.simboot.annotation.FilterValue
 import love.forte.simboot.filter.MatchType
@@ -27,12 +28,22 @@ class GroupListener(
     private val botConfig: BotConfig
 ) {
 
-    @Filter("抖音解析{{videoUrl}}", matchType = MatchType.REGEX_MATCHES)
+    @Filter(value = "\\d\\.\\d{2} [a-zA-Z]{3}:/.*", matchType = MatchType.REGEX_MATCHES)
+    @BotListener(time = 10, timeUnit = TimeUnit.SECONDS)
+    suspend fun GroupMessageEvent.douyin1() {
+        val url = ReUtil.get("https://v\\.douyin\\.com/\\w{7}/", messageContent.plainText, 0)
+        if (!url.isNullOrEmpty()) {
+            bot.launch { send("检测到抖音视频链接，正在进行智能解析~") }
+            douyin(url)
+        }
+    }
+
+    @Filter(value = "抖音解析{{videoUrl}}", matchType = MatchType.REGEX_MATCHES)
     @BotListener(time = 10, timeUnit = TimeUnit.SECONDS)
     suspend fun GroupMessageEvent.douyin(@FilterValue("videoUrl") videoUrl: String) {
         val url = ReUtil.get("https://v\\.douyin\\.com/\\w{7}/", videoUrl, 0)
         if (url.isNullOrEmpty()) {
-            send("没有找到待解析的视频链接，抖音解析+[分享链接]即可解析~")
+            bot.launch { send("没有找到待解析的视频链接，抖音解析+[分享链接]即可解析~") }
             return
         }
         val response = HttpUtil.createPost(botConfig.api!!.douyin)
@@ -47,28 +58,30 @@ class GroupListener(
                 val authorCover =
                     data.getJSONObject("author")?.getJSONObject("avatar_medium")
                         ?.getJSONArray("url_list")?.getString(0)
-                lateinit var messages: Messages
-                data.getJSONObject("video")?.apply {
+
+                val videoMsg: Messages? = data.getJSONObject("video")?.run {
                     val videoPlayAddr = String.format(
                         "https://aweme.snssdk.com/aweme/v1/play/?video_id=%s&ratio=1080p&line=0",
                         getJSONObject("play_addr").getString("uri")
                     )
                     val videoCoverAddr = getJSONObject("cover").getJSONArray("url_list").getString(0)
-                    messages = messages(
+                    messages(
                         videoCoverAddr.toImage(),
                         desc.toText() + "\n",
                         videoPlayAddr.toText()
                     )
                 }
+                lateinit var imgList: MutableList<String>
                 data.getJSONObject("image")?.apply {
-                    messages = messages("".toText())
+                    imgList = ArrayList()
                     with(getJSONArray("images")) {
+                        if (size > 5) {
+                            bot.launch { send("待解析的资源过多，请耐心等待~") }
+                        }
                         for (i in 0 until size) {
-                            val imageUrl = getJSONObject(i).getJSONArray("url_list").getString(0)
-                            messages += messages(imageUrl.toImage(), imageUrl.toText())
+                            imgList.add(getJSONObject(i).getJSONArray("url_list").getString(0))
                         }
                     }
-                    messages += messages("\n$desc".toText())
                 }
                 send(buildMiraiForwardMessage {
                     bot.says("【抖音解析】")
@@ -82,7 +95,42 @@ class GroupListener(
                                 ?: "暂未获取到信息\n".toText()
                         )
                     )
-                    bot.says(messages)
+                    if (videoMsg != null) {
+                        bot.says(videoMsg)
+                    }
+                    if (imgList.isEmpty().not()) {
+                        bot.says(messages(desc.toText()))
+                        // 分组容量
+                        val groupSize = 15
+                        if (imgList.size < groupSize) {
+                            for (i in 0 until imgList.size) {
+                                bot.says(messages(imgList[i].toImage(), imgList[i].toText()))
+                            }
+                        } else {
+                            // 分组发送
+                            for (i in 0 until groupSize) {
+                                bot.says(messages(imgList[i].toImage(), imgList[i].toText()))
+                            }
+
+                            for (i in groupSize until imgList.size step groupSize) {
+                                bot.launch {
+                                    send(
+                                        buildMiraiForwardMessage {
+                                            if (imgList.size - i < groupSize) {
+                                                for (j in i until imgList.size) {
+                                                    bot.says(messages(imgList[j].toImage(), imgList[j].toText()))
+                                                }
+                                            } else {
+                                                for (j in i until i + groupSize) {
+                                                    bot.says(messages(imgList[j].toImage(), imgList[j].toText()))
+                                                }
+                                            }
+                                        }
+                                    )
+                                }
+                            }
+                        }
+                    }
                 })
             }
 
